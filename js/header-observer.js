49c6b389ddf2846ff1b1656f32a213f62e69632c
Prateek Singh <prateeks@synchronisys.com>
2020-10-24 17:57:04 +0530

    Initial Nav and design layout added

---
+++ js/header-observer.js
@@ -0,0 +1,22 @@
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".hero");
+
+const sectionOneOptions = {
+  rootMargin: "-100px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("header__solid");
+    } else {
+      header.classList.remove("header__solid");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
