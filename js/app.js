cdd72f5f18297ec23944c2806ea74bb5ee4ba2f2
Prateek Singh <prateeks@synchronisys.com>
2020-11-23 17:23:37 +0530

    sw assets path updated

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ overlay.addEventListener('click', () => {
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('sw.js')
+        navigator.serviceWorker.register('/{techinnovators}/sw.js')
             .then((reg) => console.log('service worker registered', reg))
             .catch((err) => console.log('service worker not registered', err));
     });
